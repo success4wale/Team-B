e3f34e2026fa09fd4c33ba559450217671067182
Sunee Patchava <Suneela.Patchava@gmail.com>
2020-06-30 23:12:34 -0500

    created POJO

---
+++ employeeDetails/src/main/java/com/teamBProg/employeeDetails/App.java
@@ -0,0 +1,47 @@
+package com.teamBProg.employeeDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//Master Branch 
+    	//Testing Purpose
+    	Employee e1 = new Employee();
+    	e1.setName("ram");
+    	e1.setId(76);
+    	e1.setSalary(8900.5);
+    	
+    	Employee e2 = new Employee();
+    	e2.setName("Sam");
+    	e2.setId(70);
+    	e2.setSalary(8970.5);
+    	
+    	Employee e3 = new Employee();
+    	e3.setName("deep");
+    	e3.setId(1);
+    	e3.setSalary(90000);
+    	
+    	List<Employee>employees =new ArrayList<>();
+    	employees.add(e1);
+    	employees.add(e2);
+    	employees.add(e3);
+    	
+    	for(Employee e:employees) {
+    		System.out.println("Employee Details ");
+    		System.out.println("Name :"+ e.getName());
+    		System.out.println("id :" +e.getId());
+    		System.out.println("salary :" + e.getSalary());
+    		System.out.println("*******************");
+    	}
+    	
+    
+    }
+    
+}
---
+++ employeeDetails/src/main/java/com/teamBProg/employeeDetails/Employee.java
@@ -0,0 +1,47 @@
+package com.teamBProg.employeeDetails;
+
+public class Employee 
+{
+	
+	public String name;
+	public int id;
+	public double salary;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the salary
+	 */
+	public double getSalary() {
+		return salary;
+	}
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+
+}
